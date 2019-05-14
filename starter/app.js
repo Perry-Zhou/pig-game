b832eab6fdf900a3de04f7975b08f1b3f1ecb245
Perry Zhou <zhouperry4@gmail.com>
2019-05-14 15:12:56 -0700

    added button

--- starter/app.js
+++ starter/app.js
@@ -24,3 +24,10 @@ var x = document.querySelector('#score-0').textContent;
 console.log(x);
 
 document.querySelector('.dice').style.display ='none';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    var dice = Math.floor(Math.random() + 6) + 1;
+
+    document.querySelector('.dice').style.display = 'block';
+});
