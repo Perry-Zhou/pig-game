b41c57f10aff18946b99ce1dd2b76e661746df5f
Perry Zhou <zhouperry4@gmail.com>
2019-05-27 17:34:49 -0700

    Added more functionality to buttons

--- starter/app.js
+++ starter/app.js
@@ -9,11 +9,11 @@ PIG BOSS RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 // dice = Math.floor(Math.random() *6) +1;
 //
@@ -25,14 +25,46 @@ activePlayer = 1;
 
 document.querySelector('.dice').style.display ='none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // 1. Random number
-    var dice = Math.floor(Math.random() + 6) + 1;
+    var dice = Math.floor(Math.random() * 6) + 1;
   // 2. Display the results
     var diceDOM = document.querySelector('.dice');
     diceDOM.style.display = 'block';
-    diceDOM.srs = 'dice-' + dice + '-png'; 
-    document.querySelector('.dice').style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    // document.querySelector('.dice').style.display = 'block';
   //3. Update the round score IF the rolled number was NOT a 1
 
+
+    if (dice !== 1) {
+      //Add scores
+      roundScore += dice;
+      document.querySelector('#current-' + activePlayer).textContent = roundScore;
+      //^=roundScore = roundScore + dice
+  } else {
+      //Next Player
+      activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+      roundScore = 0;
+      // ^if(activePlayer ===0) {
+      //     activePlayer = 1;
+      // } else {
+      //     activePlayer = 0;
+      // }
+      document.getElementById('current-0').textContent = '0';
+      document.getElementById('current-1').textContent = '0';
+
+      document.querySelector('.player-0-panel').classList.toggle('active');
+      document.querySelector('.player-1-panel').classList.toggle('active');
+
+      document.querySelector('.dice').style.display = 'none';
+
+      // document.querySelector('.player-0-panel').classList.remove('active');
+      // document.querySelector('.player-1-panel').classList.remove('active');
+
+  }
 });
